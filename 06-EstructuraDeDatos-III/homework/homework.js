d664504186211b20b56461f7597f983ed465406d
Facundo Besio <facubesio.08@gmail.com>
2023-12-13 19:25:42 -0300

    06 - EstructuraDeDatos III - Homework Complete

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -9,7 +9,95 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+   this.value = value;
+   this.left = null;
+   this.right = null;
+}
+
+// ! INSERT:
+BinarySearchTree.prototype.insert =
+   function (value) {
+      if (value < this.value) {
+         if (!this.left) {
+            const newTree = new BinarySearchTree(value)
+            this.left = newTree
+         } else {
+            this.left.insert(value)
+         }
+      } else if (value >= this.value) {
+         if (!this.right) {
+            const newTree = new BinarySearchTree(value)
+            this.right = newTree
+         } else {
+            this.right.insert(value)
+         }
+      }
+   }
+
+// ! CONTAINS:
+BinarySearchTree.prototype.contains =
+   function (value) {
+      if (value === this.value) return true;
+      if (value < this.value && this.left !== null) {
+         return this.left.contains(value)
+      }
+      if (value > this.value && this.right !== null) {
+         return this.right.contains(value)
+      } else {
+         return false;
+      }
+   }
+
+// ! SIZE:
+BinarySearchTree.prototype.size =
+   function () {
+      // ? Caso base Nodo solo:
+      if (!this.left && !this.right) return 1;
+
+      // ? Si el Nodo tiene un solo hijo:
+      if (!this.right) return 1 + this.left.size();
+      if (!this.left) return 1 + this.right.size();
+
+      // ? Si el Nodo tiene amobos hijos:
+      return 1 + this.right.size() + this.left.size();
+   }
+
+// ! DepthFirstForEach:
+BinarySearchTree.prototype.depthFirstForEach =
+   function (cb, recorrido) {
+      // ? 'in-order' (nodo izquierdo - nodo padre - nodo derecho):
+      if (recorrido === 'in-order' || !recorrido) {
+         if (this.left) this.left.depthFirstForEach(cb, recorrido); // nodo izquierdo
+         cb(this.value); // nodo padre
+         if (this.right) this.right.depthFirstForEach(cb, recorrido); // nodo derecho
+      }
+
+      // ? 'pre-order' (nodo padre - nodo izquierdo - nodo derecho):
+      if (recorrido === 'pre-order') {
+         cb(this.value);
+         if (this.left) this.left.depthFirstForEach(cb, recorrido);
+         if (this.right) this.right.depthFirstForEach(cb, recorrido);
+      }
+
+      // ? 'post-order' (nodo izquierdo - nodo derecho - nodo padre):
+      if (recorrido === 'post-order') {
+         if (this.left) this.left.depthFirstForEach(cb, recorrido);
+         if (this.right) this.right.depthFirstForEach(cb, recorrido);
+         cb(this.value);
+      }
+   }
+
+// ! BreadthFirstForEach:
+// ? Recorre nodos por nivel: De izquierda a derecha y de arriba hacia abajo:
+BinarySearchTree.prototype.breadthFirstForEach =
+   function(cb, arr = []){  // arr = [] => Indica que solo si arr no esta declarado, cree por default uno vacio.
+      cb(this.value);
+      this.left && arr.push(this.left);
+      this.right && arr.push(this.right);
+
+      arr.length && arr.shift().breadthFirstForEach(cb, arr);
+   }
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
